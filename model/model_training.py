fa412ac3a58e716c7bb39680553b02389a4dbfd8
a8752578a <lukacsanna347@gmail.com>
2024-11-28 21:04:37 +0100

    random forest classifier and silhouette score added

--- model/model_training.py
+++ model/model_training.py
@@ -7,7 +7,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import RandomizedSearchCV, cross_val_score
 import joblib
 import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils import resample
 import time
+from sklearn.metrics import silhouette_score
 
 url = "http://influxdb:8086"  
 token = "9FEx1XT4dRY-7H65r2ByRsz-XTlvaGlMN9itr9fMWxdw_K6TK7n7skk9p-wr55aZ3rf8sWnEZ24fSrwEd7V0qQ=="  
@@ -76,8 +79,24 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 
 #Unsupervised model - KMeans
+
+# using silhouette score for determining the number of clusters
+best_score = -1
+optimal_k = 1
+
+sample_data = resample(scaled_X, n_samples=int(0.15 * len(scaled_X)), random_state=42)
+
+for k in range(2, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(sample_data)
+    score = silhouette_score(sample_data, kmeans.labels_)
+    if score > best_score:
+        best_score = score
+        optimal_k = k
+print('Number of clusters: ', k)
+
 print("Training KMeans")
-kmeans = KMeans(n_clusters=6, random_state=42)  
+kmeans = KMeans(n_clusters=k, random_state=42)  
 labels = kmeans.fit(scaled_X)
 
 joblib.dump(kmeans, 'kmeans_model.pkl')
@@ -108,9 +127,32 @@ best_xgb_model = random_search.best_estimator_
 print("Done: ", best_xgb_model)
 
 cv_scores = cross_val_score(best_xgb_model, scaled_X, y, cv=5, scoring='accuracy')
-print(f"Cross-validation Accuracy: {cv_scores.mean():.4f} (+/- {cv_scores.std():.4f})")
+print(f"XGBoost Cross-validation Accuracy: {cv_scores.mean():.4f} (+/- {cv_scores.std():.4f})")
 
 joblib.dump(xgb_model, 'xgb_model.pkl')
+
+# Random Forest
+
+# Find feature importances by training the model on all the features
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf.fit(X, y)
+
+importances = rf.feature_importances_
+
+# Select features based on importance
+threshold = np.mean(importances)
+selected_features = X.columns[importances > threshold]
+X_selected = X[selected_features]
+
+# Train with the selected features
+rf_selected = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_selected.fit(X_selected, y)
+
+cv_scores = cross_val_score(rf_selected, X_selected, y, cv=5)
+print(f"Random Forest Cross-validation Accuracy: {cv_scores.mean():.4f} (+/- {cv_scores.std():.4f})")
+
+joblib.dump(rf_selected, 'rf_model.pkl')
+print("Random Forest trained and saved!")
 
 '''
     TO-DOs: it seems this files re-runs after it finishes and sometimes gets az APIException error 
